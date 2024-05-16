ecbd41c6d72d56fe06def36ad6427acf2e3b9768
caitlyn <catbouce@gmail.com>
2024-05-16 14:26:39 -0700

    configure Test database

--- src/main/java/com/revature/utilities/ConnectionUtil.java
+++ src/main/java/com/revature/utilities/ConnectionUtil.java
@@ -1,22 +1,29 @@
 package com.revature.utilities;
 
+import com.revature.MainDriver;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
-    
-    public static Connection createConnection() throws SQLException{
-        return DriverManager.getConnection("jdbc:sqlite:src/main/resources/planetarium.db");
-    }
 
-    public static void main(String[] args) {
-        // run this to create database
+    public static Connection createConnection() throws SQLException {
+        InputStream props = MainDriver.class.getClassLoader().getResourceAsStream("database.properties");
+        Properties properties = new Properties();
         try {
-            ConnectionUtil.createConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
+            properties.load(props);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        boolean useTest = Boolean.parseBoolean(properties.getProperty("test-mode"));
+        if (useTest) {
+            return DriverManager.getConnection("jdbc:sqlite:src/test/resources/PlanetariumForTest.db");
+        }
+        return DriverManager.getConnection("jdbc:sqlite:src/main/resources/planetarium.db");
     }
 
 }
